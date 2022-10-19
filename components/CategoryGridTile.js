9320f82f65f8629c8377f4d343e1a254d5538779
Stephen Moore <judo2000@comcast.net>
2022-10-19 15:54:17 -0600

    Create 2 columns to display initial grid using FlatList

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,10 +1,10 @@
-import { Pressable, View, Text } from 'react-native';
+import { Pressable, View, Text, StyleSheet } from 'react-native';
 
 const CategoryGridTile = ({ title, color }) => {
   return (
-    <View>
-      <Pressable>
-        <View>
+    <View style={styles.gridItem}>
+      <Pressable style={styles.button}>
+        <View style={styles.innerContainer}>
           <Text>{title}</Text>
         </View>
       </Pressable>
@@ -13,3 +13,27 @@ const CategoryGridTile = ({ title, color }) => {
 };
 
 export default CategoryGridTile;
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+    backgroundColor: 'white',
+    shadowColor: 'black',
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+  },
+  button: {
+    flex: 1,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
