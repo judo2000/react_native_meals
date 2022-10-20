3d5d859c530a0f709383b68d0bd7c49d6801b055
Stephen Moore <judo2000@comcast.net>
2022-10-20 15:18:17 -0600

    Style grid blocks

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,11 +1,17 @@
-import { Pressable, View, Text, StyleSheet } from 'react-native';
+import { Pressable, View, Text, StyleSheet, Platform } from 'react-native';
 
 const CategoryGridTile = ({ title, color }) => {
   return (
     <View style={styles.gridItem}>
-      <Pressable style={styles.button}>
-        <View style={styles.innerContainer}>
-          <Text>{title}</Text>
+      <Pressable
+        android_ripple={{ color: '#ccc' }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
@@ -19,21 +25,30 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 16,
     height: 150,
-    borderRadius: 8,
+    borderRadius: 20,
     elevation: 4,
     backgroundColor: 'white',
     shadowColor: 'black',
     shadowOpacity: 0.25,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
+    overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
   },
   button: {
     flex: 1,
   },
+  buttonPressed: {
+    opacity: 0.5,
+  },
   innerContainer: {
     flex: 1,
     padding: 16,
+    borderRadius: 20,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
   },
 });
